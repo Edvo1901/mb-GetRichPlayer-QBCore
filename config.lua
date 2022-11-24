8882456ad3043bbdf02264f51f6adf5cff56c458
GueSS <DongVo1901@gmail.com>
2022-11-24 15:45:27 -0800

    refactor and add comment

--- config.lua
+++ config.lua
@@ -1,18 +1,39 @@
 Config = {}
 
+----------------------
+-- Log discord setup--
+----------------------
+
+--Webhook of your channel (tutorial: https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks)
 Config.WebHook = ""
+
+--Do you want the log message include the player license?
 Config.SendLogWithLicense = false
+
+--Avater for your log (optional)
 Config.AvatarURL = ""
-Config.ServerName = ""
+
+--Name for your log (optional)
+Config.ServerName = "MAYBE CHECKLOG"
+
+--Title for your log (optional)
 Config.LogTitle = "Top Richest Players"
+
+--Color for your log (optional). Default: green
 Config.LogColour = 65352
 
+--Do you want the log to be automatically sent after a certain time?
+--By the default, you need to use an admin command to trigger event
 Config.SendLogByTime = {
-    enable = false,
-    time = 1,
+    enable = false, --Enable/disable this feature
+    time = 60, --How long do you want the log to be sent (default: 60 minutes)
 }
 
+--Do you want only top richest to be sent?
+--Or you want to send money log of all players in database?
+--I added this because for larger server, you might be over 200 players and it might cause lag when log details of all of them
+--So I recommend to leave this as true
 Config.OnlyTopRichest = {
-    enable = true,
-    top = 10,
+    enable = true, --Enable/disable this feature
+    top = 10, --How many top players you want to log? (By default: Top 10 richest players)
 }
--- server/main.lua
+++ server/main.lua
@@ -1,62 +1,19 @@
 QBCore = exports['qb-core']:GetCoreObject()
 
-Citizen.CreateThread(function()
-    while (Config.SendLogByTime.enable) do
-        Citizen.Wait(Config.SendLogByTime.time * (60 * 1000))
-        TriggerEvent("mb-GetRichPlayer:server:sendLog")
-    end
-end)
-
-RegisterNetEvent("mb-GetRichPlayer:server:getTopPlayerMoney", function(license)
-    if (license) then
-        local topRichestPlayers = MySQL.Sync.fetchAll("SELECT `name`, `money`, `citizenid`, `license`, JSON_VALUE(money, '$.cash') + JSON_VALUE(money, '$.bank') AS `total_money` FROM `players` GROUP BY `name` ORDER BY `total_money` DESC LIMIT ?", {Config.OnlyTopRichest.top})
-        local resultWithLicense = Lang:t("message.top_with_license", {name = topRichestPlayers[1]["name"], citizenid = topRichestPlayers[1]["citizenid"], license = topRichestPlayers[1]["license"], money = topRichestPlayers[1]["money"], totalMoney = topRichestPlayers[1]["total_money"]})
-
-        sendToDiscord(resultWithLicense)
-    else
-        local topRichestPlayers = MySQL.Sync.fetchAll("SELECT `name`, `money`, `citizenid`, JSON_VALUE(money, '$.cash') + JSON_VALUE(money, '$.bank') AS `total_money` FROM `players` GROUP BY `name` ORDER BY `total_money` DESC LIMIT ?", {Config.OnlyTopRichest.top})
-        local resultWithoutLicense = Lang:t("message.top_without_license", {name = topRichestPlayers[1]["name"], citizenid = topRichestPlayers[1]["citizenid"], money = topRichestPlayers[1]["money"], totalMoney = topRichestPlayers[1]["total_money"]})
-
-        sendToDiscord(resultWithoutLicense)
-    end
-end)
-
-RegisterNetEvent("mb-GetRichPlayer:server:getAllPlayerMoney", function(license)
-    if (license) then
-        local topRichestPlayers = MySQL.Sync.fetchAll("SELECT `name`, `money`, `citizenid`, `license`, JSON_VALUE(money, '$.cash') + JSON_VALUE(money, '$.bank') AS `total_money` FROM `players` GROUP BY `name` ORDER BY `total_money` DESC")
-        local resultWithLicense = Lang:t("message.top_with_license", {name = topRichestPlayers[1]["name"], citizenid = topRichestPlayers[1]["citizenid"], license = topRichestPlayers[1]["license"], money = topRichestPlayers[1]["money"], totalMoney = topRichestPlayers[1]["total_money"]})
-
-        sendToDiscord(resultWithLicense)
-    else
-        local topRichestPlayers = MySQL.Sync.fetchAll("SELECT `name`, `money`, `citizenid`, JSON_VALUE(money, '$.cash') + JSON_VALUE(money, '$.bank') AS `total_money` FROM `players` GROUP BY `name` ORDER BY `total_money` DESC LIMIT ?")
-        local resultWithoutLicense = Lang:t("message.top_without_license", {name = topRichestPlayers[1]["name"], citizenid = topRichestPlayers[1]["citizenid"], money = topRichestPlayers[1]["money"], totalMoney = topRichestPlayers[1]["total_money"]})
-
-        sendToDiscord(resultWithoutLicense)
-    end
-end)
-
-RegisterNetEvent("mb-GetRichPlayer:server:sendLog", function()
-    if (Config.OnlyTopRichest.enable) then
-        TriggerEvent("mb-GetRichPlayer:server:getTopPlayerMoney", Config.SendLogWithLicense)
-    else
-        TriggerEvent("mb-GetRichPlayer:server:getAllPlayerMoney", Config.SendLogWithLicense)
-    end
-end)
-
-function sendToDiscord(message)
+function sendToDiscord(message) --Functions to send the log to discord
     local time = os.date("*t")
 
     local embed = {
             {
-                ["color"] = Config.LogColour,
+                ["color"] = Config.LogColour, --Set color
                 ["author"] = {
-                    ["icon_url"] = Config.AvatarURL,
-                    ["name"] = Config.ServerName,
+                    ["icon_url"] = Config.AvatarURL, -- et avatar
+                    ["name"] = Config.ServerName, --Set name
                 },
-                ["title"] = "**".. Config.LogTitle .."**",
-                ["description"] = message,
+                ["title"] = "**".. Config.LogTitle .."**", --Set title
+                ["description"] = message, --Set message
                 ["footer"] = {
-                    ["text"] = '' ..time.year.. '/' ..time.month..'/'..time.day..' '.. time.hour.. ':'..time.min,
+                    ["text"] = '' ..time.year.. '/' ..time.month..'/'..time.day..' '.. time.hour.. ':'..time.min, --Get time
                 },
             }
         }
@@ -64,6 +21,14 @@ function sendToDiscord(message)
     PerformHttpRequest(Config.WebHook, function(err, text, headers) end, 'POST', json.encode({username = name, embeds = embed}), { ['Content-Type'] = 'application/json' })
 end
 
+Citizen.CreateThread(function()
+    while (Config.SendLogByTime.enable) do
+        Citizen.Wait(Config.SendLogByTime.time * (60 * 1000)) --Wait time before trigger the event
+        TriggerEvent("mb-GetRichPlayer:server:sendLog") --Trigger sending log event
+    end
+end)
+
+--Admin command to trigger the event without waititng for the timer or simply you disable the auto log feature
 QBCore.Commands.Add(Lang:t("command.name"), Lang:t("command.help"), {}, false, function()
     if (Config.OnlyTopRichest.enable) then
         TriggerEvent("mb-GetRichPlayer:server:getTopPlayerMoney", Config.SendLogWithLicense)
@@ -71,3 +36,42 @@ QBCore.Commands.Add(Lang:t("command.name"), Lang:t("command.help"), {}, false, f
         TriggerEvent("mb-GetRichPlayer:server:getAllPlayerMoney", Config.SendLogWithLicense)
     end
 end, 'admin')
+
+--Event for TOP RICHEST ONLY
+RegisterNetEvent("mb-GetRichPlayer:server:getTopPlayerMoney", function(license)
+    if (license) then
+        local topRichestPlayers = MySQL.Sync.fetchAll("SELECT `name`, `money`, `citizenid`, `license`, JSON_VALUE(money, '$.cash') + JSON_VALUE(money, '$.bank') AS `total_money` FROM `players` GROUP BY `name` ORDER BY `total_money` DESC LIMIT ?", {Config.OnlyTopRichest.top})
+        local resultWithLicense = Lang:t("message.top_with_license", {name = topRichestPlayers[1]["name"], citizenid = topRichestPlayers[1]["citizenid"], license = topRichestPlayers[1]["license"], money = topRichestPlayers[1]["money"], totalMoney = topRichestPlayers[1]["total_money"]})
+
+        sendToDiscord(resultWithLicense) --Send log to discord
+    else
+        local topRichestPlayers = MySQL.Sync.fetchAll("SELECT `name`, `money`, `citizenid`, JSON_VALUE(money, '$.cash') + JSON_VALUE(money, '$.bank') AS `total_money` FROM `players` GROUP BY `name` ORDER BY `total_money` DESC LIMIT ?", {Config.OnlyTopRichest.top})
+        local resultWithoutLicense = Lang:t("message.top_without_license", {name = topRichestPlayers[1]["name"], citizenid = topRichestPlayers[1]["citizenid"], money = topRichestPlayers[1]["money"], totalMoney = topRichestPlayers[1]["total_money"]})
+
+        sendToDiscord(resultWithoutLicense) --Send log to discord
+    end
+end)
+
+--Event for ALL PLAYER
+RegisterNetEvent("mb-GetRichPlayer:server:getAllPlayerMoney", function(license)
+    if (license) then
+        local topRichestPlayers = MySQL.Sync.fetchAll("SELECT `name`, `money`, `citizenid`, `license`, JSON_VALUE(money, '$.cash') + JSON_VALUE(money, '$.bank') AS `total_money` FROM `players` GROUP BY `name` ORDER BY `total_money` DESC")
+        local resultWithLicense = Lang:t("message.top_with_license", {name = topRichestPlayers[1]["name"], citizenid = topRichestPlayers[1]["citizenid"], license = topRichestPlayers[1]["license"], money = topRichestPlayers[1]["money"], totalMoney = topRichestPlayers[1]["total_money"]})
+
+        sendToDiscord(resultWithLicense) --Send log to discord
+    else
+        local topRichestPlayers = MySQL.Sync.fetchAll("SELECT `name`, `money`, `citizenid`, JSON_VALUE(money, '$.cash') + JSON_VALUE(money, '$.bank') AS `total_money` FROM `players` GROUP BY `name` ORDER BY `total_money` DESC LIMIT ?")
+        local resultWithoutLicense = Lang:t("message.top_without_license", {name = topRichestPlayers[1]["name"], citizenid = topRichestPlayers[1]["citizenid"], money = topRichestPlayers[1]["money"], totalMoney = topRichestPlayers[1]["total_money"]})
+
+        sendToDiscord(resultWithoutLicense) --Send log to discord
+    end
+end)
+
+--Auto send log to discord
+RegisterNetEvent("mb-GetRichPlayer:server:sendLog", function()
+    if (Config.OnlyTopRichest.enable) then
+        TriggerEvent("mb-GetRichPlayer:server:getTopPlayerMoney", Config.SendLogWithLicense)
+    else
+        TriggerEvent("mb-GetRichPlayer:server:getAllPlayerMoney", Config.SendLogWithLicense)
+    end
+end)
